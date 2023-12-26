142ced4a1913cb8cf5ddc13c2288011110430f13
raj_mistry01 <makwanaraj782@gmail.com>
2023-12-26 18:57:56 +0530

    datatype_conversion_functions.c

---
+++ datatype_conversion_functions.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+    double x = 97.0;
+    int y = (int)x;
+    printf("int y=%d\n", y);
+    y = (float)x;
+    printf("float y=%f\n", y);
+    y = (char)x;
+    printf("char y=%c", y);
+}
+// Use of int ,float,char and double function.
