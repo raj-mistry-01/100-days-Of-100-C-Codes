70ae03c1819edb635c03ea79521608254f20d353
raj_mistry01 <makwanaraj782@gmail.com>
2023-12-26 21:32:05 +0530

    escape_sequences.c

---
+++ escape_sequences.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main()
+{
+    printf("I am \n lucky\n");
+    printf(" \t I am lucky\n");
+    printf("I am vey \"lucky\"\n ");
+    printf("I am very \'lucky\'\n");
+    printf("\\ I am very lucky\n");
+    printf("Am I very lucky\?\n");
+    printf("I am very \b\b\b\b\b lucky");
+    printf("I am very \b\b lucky\n");
+    printf("Amvery\rLucky\n");
+}
+// understand each \ by the output of the code.
